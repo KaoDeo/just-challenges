401fd19dc479219e49a5c000bc3fb81e63a1eddf
Ana Kasrashvili <91371368+KaoDeo@users.noreply.github.com>
2022-04-13 18:46:47 +0400

    docs: added discription

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+// Write a function that joins these strings together such that they form the following words:
+// 'Javascript', 'Countryside', and 'Downtown'
+
+// myFunction('java', 'tpi%rcs')  Expected 'Javascript'
+// myFunction('c%ountry', 'edis') Expected 'Countryside'
+// myFunction('down', 'nw%ot')    Expected 'Downtown'
+
 function myFunction(a, b) {
 let capitalA = a.slice(0,1).toUpperCase()
 let cleanA = a.replace(a[0], capitalA).replace('%','')
